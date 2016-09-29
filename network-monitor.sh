c4ac38675ca51209ef43cfce8f68c4d00418a662
changchuming <changchuming@gmail.com>
2016-09-29 21:49:52 +0800

    Changed gitignore

--- network-monitor.sh
+++ network-monitor.sh
@@ -6,8 +6,10 @@ sudo ping -c4 ${TESTIP} > /dev/null
 
 if [ $? != 0 ]
 then
-    logger -t $0 "WiFi has gone down- reboot"
-    sudo ifdown wlan0
+    logger -t $0 "WiFi has gone down - restart"
+    sudo ifdown --force wlan0
+    sudo service dhcpcd restart
+    sudo service ssh restart
     sudo ifup wlan0
 else
     logger -t $0 "WiFi is currently ok"
