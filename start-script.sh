82e855ca52f5a69481fafb721d8ffc3233c28d24
changchuming <changchuming@gmail.com>
2016-10-18 12:54:06 +0800

    Changed name of server file in autostart

--- start-script.sh
+++ start-script.sh
@@ -6,7 +6,7 @@ gpio mode 15 out
 gpio write 15 1
 gpio mode 16 out
 gpio write 16 1
-sudo forever start "/home/pi/home-automation/automation-client/app.js"
+sudo forever start "/home/pi/home-automation/automation-client/automate.js"
 cd /home/pi/home-automation/webcam-server
 ./motion -n -c motion-mmalcam.conf &
 
