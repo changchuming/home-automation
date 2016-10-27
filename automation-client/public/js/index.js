bc0b5cb3e7ea9979b38d23d0c8b6e5df65dcfeee
Chang Chu-Ming <changchuming@gmail.com>
2016-10-27 22:48:56 +0800

    Updated camera frame on startup

--- automation-client/public/js/index.js
+++ automation-client/public/js/index.js
@@ -10,6 +10,13 @@ $(function() {
 			$('#lights input[value="' + data.lightsState + '"]').parent('.btn').addClass('active');
 			$('#fan input[value="' + data.fanState + '"]').parent('.btn').addClass('active');
 			$('#camera input[value="' + data.cameraState + '"]').parent('.btn').addClass('active');
+		  	if (data.cameraState==1) {
+		  		$('#cameraframe').show();
+		  		var currentLocation = window.location.href;
+				$("#cameraframe").attr("src", currentLocation.substring(0,currentLocation.length-1)+":8081");
+		  	} else {
+		  		$('#cameraframe').hide();
+		  	}
 		}
 	});
 });
