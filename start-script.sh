219997bcfc0deed7253e68c335e9c9060caf8a1c
Chang Chu-Ming <changchuming@gmail.com>
2016-10-27 22:49:47 +0800

    Removed camera script from startup

--- start-script.sh
+++ start-script.sh
@@ -7,6 +7,3 @@ gpio write 15 1
 gpio mode 16 out
 gpio write 16 1
 sudo forever start "/home/pi/home-automation/automation-client/automate.js"
-cd /home/pi/home-automation/webcam-server
-./motion -n -c motion-mmalcam.conf &
-
